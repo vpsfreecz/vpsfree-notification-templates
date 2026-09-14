06f03bad4294b6f28ca9478e907f43967ebeb7d0
Jakub Skokan <jakub.skokan@havefun.cz>
2026-09-14 11:34:13 +0200

    user_login_email_verification: add login-code emails
    Provide English and Czech notifications for optional verification of
    unknown login devices. Explain the fixed 30-minute deadline and resend
    behavior, identify the login request, and use the support sender addresses.

---
+++ templates/user_login_email_verification/meta.rb
@@ -0,0 +1,15 @@
+template do
+  label 'Login email verification'
+
+  lang :cs do
+    from        'podpora@vpsfree.cz'
+    reply_to    'podpora@vpsfree.cz'
+    return_path 'podpora@vpsfree.cz'
+  end
+
+  lang :en do
+    from        'support@vpsfree.org'
+    reply_to    'support@vpsfree.org'
+    return_path 'support@vpsfree.org'
+  end
+end
