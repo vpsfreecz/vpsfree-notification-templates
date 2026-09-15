715c063396fa49277852b98d36347c8bec5160d3
Jakub Skokan <jakub.skokan@havefun.cz>
2026-09-15 11:19:06 +0200

    ip_release: add Czech and English notices and reminders
    Explain the planned release and current ways to retain an address without
    asking members to reply. Mention IPv4 scarcity only for public IPv4, show
    locations, and provide HTML buttons alongside the text links.
    
    Keep forced-mode notices limited to the available options and use a neutral
    closing in all variants.

---
+++ templates/ip_release_requested/meta.rb
@@ -0,0 +1,16 @@
+template :ip_release_requested do
+  label 'IP release request'
+  user_visibility true
+
+  lang :en do
+    from 'support@vpsfree.org'
+    reply_to 'support@vpsfree.org'
+    return_path 'support@vpsfree.org'
+  end
+
+  lang :cs do
+    from 'podpora@vpsfree.cz'
+    reply_to 'podpora@vpsfree.cz'
+    return_path 'podpora@vpsfree.cz'
+  end
+end
